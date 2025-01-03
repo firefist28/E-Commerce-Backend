0950258ca46e16f8f7c0f96cd135ffb41a53665f
vifff <vikasalex1996@gmail.com>
2025-01-03 14:32:42 +0530

    mongo db connectivity issue fix

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const connectDB = async () => {
     try {
         console.log('mongo db ' + process.env.MONGO_URI);
-        await mongoose.connect('mongodb://localhost:27017/e-commerce');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected...");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error.message);
